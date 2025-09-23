4375b773e7a16c7ac12f9c22ce43fc527580378f
Josefinx <josefin.wetterberg@gmail.com>
2025-09-23 10:24:20 +0200

    added configurator components and started styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+
+import { useState } from "react";
 import ProductScene from "@/components/ProductScene/ProductScene";
+import Configurator from "@/components/Configurator/Configurator";
+
 
 export default function Home() {
+  const [config, setConfig] = useState({ flavour: 'orange', shape: 'diamond', color: 'blue' });
   return (
     <main>
       <ProductScene />
+      <Configurator config={config} onChange={setConfig} />
     </main>
   );
 }
